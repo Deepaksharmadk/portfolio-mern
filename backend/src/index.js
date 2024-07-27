72980c00f00a3342914a4c176265f6e4190b6e12
deepaksharmadk <dks847203@gmail.com>
2024-07-27 23:55:11 +0530

    connect to database

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import connectDB from "./db/dataBaseConnection.js";
 
 dotenv.config();
 const app = express();
@@ -14,5 +15,8 @@ app.get("/health", (req, res) => {
   res.send("Hello World!");
 });
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log("Server is running on port 5000"));
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
