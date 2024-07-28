d1672534fd8e6306401212d5d17cf88767ea3881
deepaksharmadk <dks847203@gmail.com>
2024-07-28 13:56:29 +0530

    add to multer and cloudinary for file upload

---
+++ backend/src/utils/multer.js
@@ -0,0 +1,24 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({
+  storage,
+});
+export const uploadFile = upload.fields([
+  {
+    name: "avatar",
+    maxCount: 1,
+  },
+  {
+    name: "resume",
+    maxCount: 1,
+  },
+]);
