90d762e513ca796e4ad08d9b40f8216233d7891f
deepaksharmadk <dks847203@gmail.com>
2024-08-06 17:50:54 +0530

    add form model

---
+++ backend/src/models/Form.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const formSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+});
+const Form = mongoose.model("Form", formSchema);
+export default Form;
